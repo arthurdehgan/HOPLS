c60f3347c76255c4a2a128a423333f8b97a74bce
arthurdehgan <arthurdehgan@gmail.com>
2019-04-21 17:46:00 -0400

    last version

--- hopls.py
+++ hopls.py
@@ -7,33 +7,44 @@ import numpy as np
 from numpy.linalg import svd, pinv
 
 
-def rmse(y_true, y_pred):
+def rmsep(y_true, y_pred):
     """Compute Root Mean Square Percentage Error between two arrays."""
-    return np.sqrt(np.mean(np.square(((y_true - y_pred) / y_pred)), axis=0))
+    return np.sqrt(np.mean((y_true - y_pred) ** 2, axis=0))
+
+
+def qsquared(y_true, y_pred):
+    """Compute the Q^2 Error between two arrays."""
+    return 1 - ((tl.norm(y_true - y_pred) ** 2) / (tl.norm(y_true) ** 2))
 
 
 def cov(A, B):
-    """Computes the mode 1 (mode 0 in python) contraction of 2 matrices."""
-    assert A.shape[0] == B.shape[0], "A and B need to have the same shape on axis 0"
-    dimension_A = A.shape[1:]
-    dimension_B = B.shape[1:]
-    dimensions = list(dimension_A) + list(dimension_B)
-    rmode_A = len(dimension_A)
-    dim = A.shape[0]
-    C = np.zeros(dimensions)
-    indices = []
-    for mode in dimensions:
-        indices.append(range(mode))
-    for idx in product(indices):
-        idx_A, idx_B = list(idx[:rmode_A]), list(idx[rmode_A:])
-        C[tuple(idx_A + idx_B)] = sum(
-            [A[tuple([i] + idx_A)] * B[tuple([i] + idx_B)] for i in range(dim)]
-        )
-    return C
+    # """Computes the mode 1 (mode 0 in python) contraction of 2 matrices."""
+    # assert A.shape[0] == B.shape[0], "A and B need to have the same shape on axis 0"
+    # dimension_A = A.shape[1:]
+    # dimension_B = B.shape[1:]
+    # dimensions = list(dimension_A) + list(dimension_B)
+    # rmode_A = len(dimension_A)
+    # dim = A.shape[0]
+    # C = np.zeros(dimensions)
+    # indices = []
+    # for mode in dimensions:
+    #     indices.append(range(mode))
+    # for idx in product(indices):
+    #     idx_A, idx_B = list(idx[:rmode_A]), list(idx[rmode_A:])
+    #     C[tuple(idx_A + idx_B)] = sum(
+    #         [A[tuple([i] + idx_A)] * B[tuple([i] + idx_B)] for i in range(dim)]
+    #     )
+    # return C
+    alpha = "bcdefghijklmnopqrstuvwxyz"
+    A_alpha = alpha[: len(A.shape) - 1]
+    B_alpha = alpha[1 - len(B.shape) :]
+    string = "a" + A_alpha + "," + B_alpha + "->" + A_alpha + B_alpha
+    print(A.shape, B.shape)
+    return np.einsum("a...,a...->...", A, B)
 
 
 class HOPLS:
-    def __init__(self, R, epsilon=1e-15):
+    def __init__(self, R, Ln, Kn=None, metric=None, epsilon=0):
         """
         Parameters:
             R: int, The number of latent vectors.
@@ -47,9 +58,14 @@ class HOPLS:
                 residuals have a norm smaller than epsilon.
         """
         self.R = R
+        self.Ln = Ln
+        self.Kn = Kn
         self.epsilon = epsilon
+        if metric is None:
+            self.metric = qsquared
+        self.model = None
 
-    def _fit_2d(self, X, Y, Ln):
+    def _fit_2d(self, X, Y):
         """
         Compute the HOPLS for X and Y wrt the parameters R, Ln and Kn for the special case mode_Y = 2.
 
@@ -69,14 +85,24 @@ class HOPLS:
 
             ts: Tensor, The latent vectors of the HOPLS, of shape (i1, R).
         """
+        # Initialization
+        In = X.shape
+        M = Y.shape[-1]
         Er, Fr = X, Y
-        P, Q, G, D, T = [], [], [], [], []
+        P = []
+        Q = tl.zeros((M, self.R))
+        G = tl.zeros((self.R, *self.Ln))
+        D = tl.zeros((self.R, self.R))
+        T = tl.zeros((In[0], self.R))
         # Beginning of the algorithm
-        for i in range(self.R):
-            if tl.norm(Er) > epsilon and tl.norm(Fr) > epsilon:
-                Cr = cov(Er, Fr)
+        for r in range(self.R):
+            if tl.norm(Er) > self.epsilon and tl.norm(Fr) > self.epsilon:
+                Cr = np.einsum("abcd,ax->bcdx", Er, Fr)
                 # HOOI ticker decomposition of C
-                Gr_C, latents = tucker(Cr, ranks=[1] + Ln)
+                # print(len(self.Ln), len(Er.shape))
+                Gr_C, latents = tucker(
+                    Cr, ranks=[1] + self.Ln, n_iter_max=int(1e6), tol=1e-7
+                )
                 # Getting P and Q
                 qr = latents[0]
                 Pr = latents[1:]
@@ -86,34 +112,24 @@ class HOPLS:
                 tr = np.matmul(tl.unfold(tr, 0), pinv(Gr_C.reshape(1, -1)))
                 tr /= tl.norm(tr)
                 # recomposition of the core tensors
-                G.append(tl.tucker_to_tensor(Er, [tr.T] + [pn.T for pn in Pr]))
+                Gr = tl.tucker_to_tensor(Er, [tr.T] + [pn.T for pn in Pr])
                 ur = np.matmul(Fr, qr)
                 dr = np.matmul(ur.T, tr)
                 # Deflation
-                Er = Er - tl.tucker_to_tensor(G[i], [tr] + Pr)
+                Er = Er - tl.tucker_to_tensor(Gr, [tr] + Pr)
                 Fr = Fr - dr * np.matmul(tr, qr.T)
                 # Gathering of R variables
                 P.append(Pr)
-                Q.append(qr)
-                D.append(dr)
-                T.append(tr)
+                Q[:, r] = qr.T
+                G[r] = Gr[0]
+                D[r, r] = dr
+                T[:, r] = tr[:, 0]
             else:
                 break
-        # reshaping the loadings
-        P = tl.tensor(P)
-        Q = tl.tensor(Q)
-        # P = [P[:, i] for i in range(P.shape[1])]
-        # Q = [Q[:, i] for i in range(Q.shape[1])]
-        self.model = (
-            tl.tensor(G).squeeze(),
-            P,
-            tl.tensor(D).squeeze(),
-            Q,
-            tl.tensor(T).squeeze(),
-        )
+        self.model = (P, Q, G, D, T)
         return self.model
 
-    def fit(self, X, Y, Ln, Kn=None):
+    def fit(self, X, Y):
         """
         Compute the HOPLS for X and Y wrt the parameters R, Ln and Kn.
 
@@ -139,22 +155,24 @@ class HOPLS:
         assert Y_mode >= 2, "Y need to be mode 2 minimum."
         assert X_mode >= 3, "X need to be mode 3 minimum."
         assert (
-            len(Ln) == X_mode - 1
+            len(self.Ln) == X_mode - 1
         ), f"The list of ranks for the decomposition of X (Ln) need to be equal to the mode of X -1: {X_mode-1}."
         if Y_mode == 2:
-            return self._fit_2d(X, Y, Ln)
+            return self._fit_2d(X, Y)
         assert (
-            len(Kn) == Y_mode - 1
-        ), f"The list of ranks for the decomposition of Y (Ln) need to be equal to the mode of Y -1: {Y_mode-1}."
+            len(self.Kn) == Y_mode - 1
+        ), f"The list of ranks for the decomposition of Y (Kn) need to be equal to the mode of Y -1: {Y_mode-1}."
         # Initialization
         Er, Fr = X, Y
         P, Q, G, D, T = [], [], [], [], []
         # Beginning of the algorithm
         for i in range(self.R):
-            if tl.norm(Er) > epsilon and tl.norm(Fr) > epsilon:
-                Cr = cov(Er, Fr)
+            if tl.norm(Er) > self.epsilon and tl.norm(Fr) > self.epsilon:
+                Cr = np.tensordot(Er, Fr, (0, 0))
                 # HOOI ticker decomposition of C
-                _, latents = tucker(Cr, ranks=Ln + Kn)
+                _, latents = tucker(
+                    Cr, ranks=self.Ln + self.Kn, n_iter_max=int(1e6), tol=1e-7
+                )
                 # Getting P and Q
                 Pr = latents[: len(Er.shape) - 1]
                 Qr = latents[len(Er.shape) - 1 :]
@@ -203,29 +221,27 @@ class HOPLS:
         Returns:
             Y_pred: tensorly Tensor, The predicted Y from the model.
         """
-        g, p, d, q, ts = self.model
-        N = p.shape[1]
-        gp = [pinv(g[k]) for k in range(self.R)]
-        wr = [
-            np.matmul(kronecker([p[k, N - i - 1] for i in range(N)]), gp[k].flatten())
-            for k in range(self.R)
-        ]
-        qr = [
-            np.matmul(d[k].flatten(), kronecker([q[k, N - i - 1] for i in range(N)]).T)
-            for k in range(self.R)
-        ]
-        W_star = np.asarray(wr).T
-        Q_star = np.asarray(qr).T
-        X = tl.unfold(X, 0)
-        X = np.matmul(X, W_star)
-        if len(Y.shape) > 2:
-            return np.matmul(X, Q_star.T).reshape(Y.shape)
-        elif len(Y.shape) == 2:
-            return np.matmul(np.matmul(X, np.diag(d)), q.squeeze())
+        P, Q, G, D, _ = self.model
+        N = len(self.Ln)
+        G_pi = tl.zeros(G.shape)
+        Q_star = tl.zeros(Q.shape)
+        W = tl.zeros((*X.shape[1:], self.R)).reshape(-1, self.R)
+        for r in range(self.R):
+            G_pi[r] = pinv(G[r])
+            W[:, r] = np.matmul(
+                kronecker([P[r][N - n - 1] for n in range(N)]), G_pi[r].reshape(-1)
+            )
+            if len(Y.shape) > 2:
+                Q_star[:, r] = np.matmul(
+                    tl.unfold(D[r], 0), kronecker([Q[r, N - n - 1] for n in range(N)]).T
+                )
+            else:
+                Q_star[:, r] = D[r, r] * Q[:, r]
 
-    def score(self, X, Y, Ln, Kn=None, metric=None):
-        self.fit(X, Y, Ln, Kn)
+        print(tl.unfold(X, 0).shape, W.shape, Q_star.shape)
+        return np.matmul(np.matmul(tl.unfold(X, 0), W), Q_star.T)
+
+    def score(self, X, Y):
+        self.fit(X, Y)
         Y_pred = self.predict(X, Y)
-        if metric is None:
-            metric = rmse
-        return metric(Y, Y_pred)
+        return self.metric(Y, Y_pred)
--- testing.py
+++ testing.py
@@ -1,39 +1,49 @@
 from itertools import product
-import tensorly as tl
-from tensorly.decomposition import tucker
-from tensorly.tenalg.n_mode_product import mode_dot
-from tensorly.tenalg import kronecker
 import numpy as np
-from numpy.linalg import svd, pinv
-import matplotlib.pyplot as plt
+import tensorly as tl
+from tensorly.tenalg.n_mode_product import mode_dot
 from sklearn.cross_decomposition import PLSRegression
+from hopls import HOPLS, qsquared
+
+
+def hyper_params_search(data, target, metric=None, verbose=True):
+    old_error = -np.inf
+    # Just like in the paper, we simplify by having l for all ranks
+    for R, l in product(range(3, 7), range(3, 10)):
+        hopls = HOPLS(R, [l, l, l], [l, l], metric=None)
+        error = np.mean(hopls.score(data, target))
+        if error > old_error:
+            old_error = error
+            best_params = [R, l, error]
+    if verbose:
+        print("Best model is with R={} and l={}, error={:.2f}".format(*best_params))
+    return best_params
 
 
 if __name__ == "__main__":
-    # arbitrarly chosen epsilon
-    epsilon = 5e-4
     # Generation according to 4.1.1 of the paper equation (29)
     T = tl.tensor(np.random.normal(size=(20, 5)))
-    P = tl.tensor(np.random.normal(size=(5, 10, 10)))
-    Q = tl.tensor(np.random.normal(size=(5, 10, 10)))
+    P = tl.tensor(np.random.normal(size=(5, 10, 13, 23)))
+    Q = tl.tensor(np.random.normal(size=(5, 15)))
     E = tl.tensor(np.random.normal(size=(20, 10, 10)))
     F = tl.tensor(np.random.normal(size=(20, 10, 10)))
-    X = mode_dot(P, T, 0)  # + epsilon * E
-    Y = mode_dot(Q, T, 0)  # + epsilon * F
-    old_mse = np.inf
+    X = mode_dot(P, T, 0)
+    Y = mode_dot(Q, T, 0)
 
-    for snr in [10, 5, 0, -5, -10]:
-        print(f"SNR={snr}")
-        epsilon = 1 / (10 ** (snr / 10))
-        noisy_X = X + epsilon * E
-        noisy_Y = Y + epsilon * F
+    hyper_params_search(X, Y, verbose=True)
 
-        # Just like in the paper, we simplify by having l for all ranks
-        for R, l in product(range(3, 7), range(3, 10)):
-            hopls = HOPLS(R)
-            rmsep = np.mean(hopls.score(noisy_X, noisy_Y, [l, l], [l, l]))
-            if rmsep < old_mse:
-                old_mse = rmsep
-                best_params = [R, l, rmsep]
-            print(rmsep)
-        print("Best model is with R={} and l={}, rmsep={:.2f}".format(*best_params))
+    # test = PLSRegression(n_components=5)
+    # test.fit(X, Y)
+    # print(qsquared(Y, test.predict(X)))
+
+    # for _ in range(100):
+    #     _, _, err = hyper_params_search(X, X, verbose=False)
+    # print(err / 100)
+
+    # for snr in [10, 5, 0, -5, -10]:
+    #     print(f"SNR={snr}")
+    #     epsilon = 1 / (10 ** (snr / 10))
+    #     noisy_X = X + epsilon * E
+    #     noisy_Y = Y  # + epsilon * F
+
+    #     hyper_params_search(noisy_X, noisy_Y)
