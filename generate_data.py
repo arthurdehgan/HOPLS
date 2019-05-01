7cced51c0a641537a293e2ae39308968ad4ea198
arthurdehgan <arthurdehgan@gmail.com>
2019-05-01 11:00:57 -0400

    generate data improved

--- generate_data.py
+++ generate_data.py
@@ -4,22 +4,32 @@ from tensorly.tenalg import mode_dot, multi_mode_dot
 from scipy.io import savemat
 
 
-def generate(I1, In, Jm, X_mode, Y_mode=2, R=5, L=7, snr=10):
-    if isinstance(In, int):
-        In = [In] * (X_mode - 1)
-    if isinstance(Jm, int):
-        Jm = [Jm] * (Y_mode - 1)
+def generate(I1, In, Jm, X_mode=None, Y_mode=2, R=5, L=7, snr=10):
+    if type(In) == tuple or type(In) == list:
+        X_mode = len(In) + 1
+    elif X_mode is None:
+        assert 1, "X_mode should not be set to None if In is an int"
+    else:
+        In = (X_mode - 1) * [In]
+
+    if type(Jm) == tuple or type(Jm) == list:
+        Y_mode = len(Jm) + 1
+    elif X_mode is None:
+        assert 1, "Y_mode should not be set to None if Jn is an int"
+    else:
+        Jm = (Y_mode - 1) * [Jm]
+
     T = tl.tensor(np.random.normal(size=(R, I1)))
-    P = []
+    P, Q = [], []
     for i in range(X_mode - 1):
         P.append(tl.tensor(np.random.normal(size=(In[i], L)).T))
-    G = tl.tensor(np.random.normal(size=[R] + [L] * (X_mode - 1)))
-    Q = []
     for i in range(Y_mode - 1):
         Q.append(tl.tensor(np.random.normal(size=(Jm[i], L)).T))
+
+    E = tl.tensor(np.random.normal(size=[I1] + list(In)))
+    F = tl.tensor(np.random.normal(size=[I1] + list(Jm)))
     D = tl.tensor(np.random.normal(size=[R] + [L] * (Y_mode - 1)))
-    E = tl.tensor(np.random.normal(size=[I1] + In))
-    F = tl.tensor(np.random.normal(size=[I1] + Jm))
+    G = tl.tensor(np.random.normal(size=[R] + [L] * (X_mode - 1)))
 
     data = multi_mode_dot(G, [T] + P, np.arange(X_mode), transpose=True)
     target = multi_mode_dot(D, [T] + Q, np.arange(Y_mode), transpose=True)
@@ -32,17 +42,12 @@ def generate(I1, In, Jm, X_mode, Y_mode=2, R=5, L=7, snr=10):
 
 
 if __name__ == "__main__":
-    R = 5
-    L = 4
-    In = [6, 7, 8]
-    Jm = 9
-    noise = 0
-    X, Y = generate(20, In, Jm, len(In) + 1, Y_mode=2, R=R, L=L, snr=noise)
-    savemat(f"data_R{R}_L{L}_X4_Y2_{noise}dB", {"X": X, "Y": Y})
-    # noise = 0
-    # modeY = 2
-    # R = 5
-    # L = 4
-    # for modeX in [3, 4, 5, 6]:
-    #     X, Y = generate(20, 10, 10, modeX, Y_mode=modeY, R=R, L=L, snr=noise)
-    #     savemat(f"data_R{R}_L{L}_X{modeX}_Y{modeY}_{noise}dB", {"X": X, "Y": Y})
+    X, Y = generate(100, (7, 8, 9), (10), 4, snr=0)
+    savemat(f"testing_data", {"X": X, "Y": Y})
+#     for order in [3, 5]:
+#         for noise in [10, 5, 0, -5]:
+#             for modY in [2, 3, 5]:
+#                 X, Y = generate(100, 10, 10, order, snr=noise)
+#                 savemat(f"data_X{order}_Y{modY}_{noise}dB", {"X": X, "Y": Y})
+
+#
