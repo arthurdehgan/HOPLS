a9a29c61e9a1a1bfd391ef6f6212dfdbead0369f
arthurdehgan <arthurdehgan@github.com>
2019-05-03 09:44:37 -0400

    added hyperparameters

--- gather_results.py
+++ gather_results.py
@@ -14,13 +14,18 @@ for X_mode, Y_mode, name, ss in product(modeX, modeY, names, sample_size):
     fPLS_scores, fNPLS_scores, fHOPLS_scores = [], [], []
     fPLS_R, fNPLS_R, fHOPLS_R = [], [], []
     fHOPLS_L = []
+    fNPLS_hyp, fHOPLS_hyp, fPLS_hyp = [], [], []
     for snr in noise:
         PLS_scores, NPLS_scores, HOPLS_scores = [], [], []
         PLS_R, NPLS_R, HOPLS_R = [], [], []
         HOPLS_L = []
+        NPLS_hyp, HOPLS_hyp, PLS_hyp = [], [], []
         for n in range(n_datasets):
             filename = f"./results/res{n}_s{ss}_X{X_mode}_Y{Y_mode}_{snr}dB.mat"
             dat = loadmat(filename)
+            PLS_hyp += [dat["PLS_hyp"]]
+            NPLS_hyp += [dat["NPLS_hyp"]]
+            HOPLS_hyp += [dat["HOPLS_hyp"]]
             PLS_scores += dat["PLS_Q2"].flatten().tolist()
             NPLS_scores += dat["NPLS_Q2"].flatten().tolist()
             HOPLS_scores += dat["HOPLS_Q2"].flatten().tolist()
@@ -28,6 +33,9 @@ for X_mode, Y_mode, name, ss in product(modeX, modeY, names, sample_size):
             NPLS_R += dat["NPLS_R"].flatten().tolist()
             HOPLS_R += dat["HOPLS_R"].flatten().tolist()
             HOPLS_L += dat["HOPLS_L"].flatten().tolist()
+        fPLS_hyp.append(PLS_hyp)
+        fNPLS_hyp.append(NPLS_hyp)
+        fHOPLS_hyp.append(HOPLS_hyp)
         fPLS_scores.append(PLS_scores)
         fNPLS_scores.append(NPLS_scores)
         fHOPLS_scores.append(HOPLS_scores)
@@ -37,11 +45,19 @@ for X_mode, Y_mode, name, ss in product(modeX, modeY, names, sample_size):
         fHOPLS_L.append(HOPLS_L)
     savemat(
         f"results/PLS_results_{name}X{X_mode}_{Y_mode}_ss{ss}",
-        {"R": np.asarray(fPLS_R), "Q2": np.asarray(fPLS_scores)},
+        {
+            "R": np.asarray(fPLS_R),
+            "Q2": np.asarray(fPLS_scores),
+            "hyp": np.asarray(fPLS_hyp),
+        },
     )
     savemat(
         f"results/NPLS_results_{name}X{X_mode}_{Y_mode}_ss{ss}",
-        {"R": np.asarray(fNPLS_R), "Q2": np.asarray(fNPLS_scores)},
+        {
+            "R": np.asarray(fNPLS_R),
+            "Q2": np.asarray(fNPLS_scores),
+            "hyp": np.asarray(fNPLS_hyp),
+        },
     )
     savemat(
         f"results/HOPLS_results_{name}X{X_mode}_{Y_mode}_ss{ss}",
@@ -49,5 +65,6 @@ for X_mode, Y_mode, name, ss in product(modeX, modeY, names, sample_size):
             "R": np.asarray(fHOPLS_R),
             "L": np.asarray(fHOPLS_L),
             "Q2": np.asarray(fHOPLS_scores),
+            "hyp": np.asarray(fHOPLS_hyp),
         },
     )
